84b316d7bf5239bb0717c005e95ad1c01df02c87
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-23 14:06:46 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,85 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/***
+ * Game where we can have to Entities battle blow-by-blow and use their magic items.
+ * NOUNS (Person, Place, Things, or Idea): Game, Entity, Item
+ * VERBS (Action words like Run or Jump):
+ * ADJECTIVES (Color, Height):
+ */
+class MagicItem{
+    String name;
+    int price = 0;
+    boolean allowMultiple = false;
+    String attr;
+    int amount;
+    MagicItem(String name, int price, String attr, int amount){
+        this.name = name;
+        this.price = price;
+        this.attr = attr;
+        this.amount = amount;
+    }
+}
+
+class Entity{
+    String name;
+    ArrayList<MagicItem> inventory;
+    HashMap<String, Integer> attrs;
+    Entity(String name){
+        this.name = name;
+        this.inventory = new ArrayList<MagicItem>();
+        this.attrs = new HashMap<String, Integer>();
+    }
+}
+class Store{
+    ArrayList<MagicItem> inventory;
+    Store(){
+        this.inventory = new ArrayList<MagicItem>();
+        this.inventory.add( new MagicItem("Fire Sword", 100, "STR", 10 ) );
+        this.inventory.add( new MagicItem("Ice Sheild", 100, "DEX", 10 ) );
+    }
+    MagicItem buy(Entity buyer, int itemIndex){
+        MagicItem item = this.inventory.get(itemIndex);
+        if(buyer.attrs.get("GOLD") >= item.price){
+            this.inventory.remove(item);
+            return item;
+        }
+        return null;
+    }
+}
+class Game{
+    ArrayList<Entity> entities;
+    Game(ArrayList<Entity> entities){
+        this.entities = entities;
+    }
+    void battle(Entity e1, Entity e2){ //fight
+        //BCS Base Chance of success +- dex armor value of your items
+        //Damage str +- attack value of item;
+        System.out.println("Battling!!!!");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Store store = new Store();
+        ArrayList<Entity> players = new ArrayList<Entity>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Entity hero = new Entity("Hero");
+        hero.attrs.put("STRENGTH", 18);
+        hero.attrs.put("DEXTERITY", 18);
+        hero.attrs.put("HEALTH", 100);
+        hero.attrs.put("GOLD", 100);
+        store.buy(hero, 1); //1 is sheild
+        players.add(hero);
+
+        Entity zombie = new Entity("Zombie");
+        zombie.attrs.put("STRENGTH", 12);
+        zombie.attrs.put("DEXTERITY", 8);
+        zombie.attrs.put("HEALTH", 50);
+        zombie.attrs.put("GOLD", 100);
+        store.buy(zombie, 0); //sword
+        players.add(zombie);
+
+        Game g = new Game(players);
+        g.battle(hero, zombie);
     }
 }
